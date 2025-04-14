dfc1bed55c0eb0c8066ac86a9be8c4c679e6f17b
yonatanst7 <104933243+yonatanst7@users.noreply.github.com>
2025-04-14 21:47:25 +0300

    created hooks and contexts

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
