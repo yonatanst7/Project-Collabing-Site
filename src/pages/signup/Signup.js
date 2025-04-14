0cc9261173b8739fc3fb650c1f9f720f4c0e2742
yonatanst7 <104933243+yonatanst7@users.noreply.github.com>
2025-04-14 23:40:47 +0300

    create signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,10 +1,69 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 // styles
 import './Signup.css'
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState(null)
   return (
-    <div className="signup">
-      <h1>Signup</h1>
-    </div>
+    <form className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <label>
+        <span>Confirm Password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
+        />
+      </label>
+
+      <label>
+        <span>Display Name:</span>
+        <input
+          type="text"
+          required
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+
+      <label>
+        <span>Profile Thumbnail:</span>
+        <input
+          type="file"
+          required
+          onChange={(e) => setThumbnail(e.target.files[0])}
+        />
+      </label>
+
+      <button className="btn">Sign up</button>
+      <p>Already have an account? <Link to="/login">Log in</Link></p>
+    </form>
   )
 }
